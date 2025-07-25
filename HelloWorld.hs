dd6da39204721a87094c170e079b8f71d7174ec8
tursenai <wildanrhomad@gmail.com>
2025-07-25 21:32:48 +0700

    Create HelloWorld.hs

---
+++ HelloWorld.hs
@@ -0,0 +1,12 @@
+{-|
+
+
+                            Online Haskell Compiler.
+                Code, Compile, Run and Debug Haskell program online.
+Write your code in this editor and press "Run" button to execute it.
+
+-}
+
+
+main :: IO ()
+main = putStrLn "Hello, world!"

cae699aca7a4eb5310ddf01f004b6bb39d7f1a0f
tursenai <wildanrhomad@gmail.com>
2025-07-25 22:05:34 +0700

    Create FromIntegral.hs

---
+++ FromIntegral.hs
@@ -0,0 +1,21 @@
+{-|
+
+
+                            Online Haskell Compiler.
+                Code, Compile, Run and Debug Haskell program online.
+Write your code in this editor and press "Run" button to execute it.
+
+-}
+
+
+main :: IO ()
+main = do
+    let a = 10
+    let b = 5
+    
+    let c = fromIntegral a / fromIntegral b  -- OK
+    -- let d = a / b  -- Error here
+    let d = a `div` b                        -- OK: integer division
+
+    print (c)
+    print (d)

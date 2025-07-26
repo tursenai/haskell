1a43b25330ee291010a55e9b5dc5688a29369cf6
tursenai <wildanrhomad@gmail.com>
2025-07-26 16:12:43 +0700

    Create BasicString.hs

---
+++ BasicString.hs
@@ -0,0 +1,14 @@
+{-|
+
+
+                            Online Haskell Compiler.
+                Code, Compile, Run and Debug Haskell program online.
+Write your code in this editor and press "Run" button to execute it.
+
+-}
+
+greeting :: String
+greeting = "Hello, " ++ "world!"
+
+main :: IO ()
+main = putStrLn greeting
